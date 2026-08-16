49602514bde9db1c99b1a51e3c8143040870b903
Fazul7506 <allanopredator687@gmail.com>
2026-08-17 01:01:12 +0700

    Envs

--- config/settings/base.py
+++ config/settings/base.py
@@ -27,7 +27,7 @@ DJANGO_APPS = [
 ]
 THIRD_PARTY_APPS = ["rest_framework", "rest_framework_simplejwt"]
 LOCAL_APPS = [
-    "anymail"
+    "anymail",
     "core",
     "trading",
     "apps.broker",
