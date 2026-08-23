98183a09d44c2562a0b3ae68a19b1d13067e29d0
Fazul7506 <allanopredator687@gmail.com>
2026-08-23 20:19:09 +0700

    Make broker provider URLs and credentials environment-driven

--- config/settings/broker.py
+++ config/settings/broker.py
@@ -1,4 +1,4 @@
-"""Broker and third-party provider settings."""
+"""Broker and third-party provider settings. All deployment-specific values come from the environment."""
 
 from .utils import env
 
@@ -7,19 +7,17 @@ TELEGRAM_CHAT_ID = env("TELEGRAM_CHAT_ID", "")
 PAYMENT_PROVIDER = env("PAYMENT_PROVIDER", "intasend").lower()
 PAYMENT_HTTP_TIMEOUT = int(env("PAYMENT_HTTP_TIMEOUT", "20"))
 
-# IntaSend (Kenya / KES / M-Pesa + card)
 INTASEND_PUBLIC_KEY = env("INTASEND_PUBLIC_KEY", "")
 INTASEND_SECRET_KEY = env("INTASEND_SECRET_KEY", "")
 INTASEND_WEBHOOK_CHALLENGE = env("INTASEND_WEBHOOK_CHALLENGE", "")
-INTASEND_API_BASE_URL = env("INTASEND_API_BASE_URL", "https://api.intasend.com")
+INTASEND_API_BASE_URL = env("INTASEND_API_BASE_URL", "https://api.intasend.com").rstrip("/")
 
-# Pesapal API 3.0
 PESAPAL_CONSUMER_KEY = env("PESAPAL_CONSUMER_KEY", "")
 PESAPAL_CONSUMER_SECRET = env("PESAPAL_CONSUMER_SECRET", "")
 PESAPAL_NOTIFICATION_ID = env("PESAPAL_NOTIFICATION_ID", "")
-PESAPAL_API_BASE_URL = env("PESAPAL_API_BASE_URL", "https://pay.pesapal.com/v3")
+PESAPAL_API_BASE_URL = env("PESAPAL_API_BASE_URL", "https://pay.pesapal.com/v3").rstrip("/")
 
 BROKER_APP_ID = env("BROKER_APP_ID", env("DERIV_APP_ID", env("DERIV_OAUTH_CLIENT_ID", "")))
-BROKER_WS_URL = env("BROKER_WS_URL", "")
+BROKER_WS_URL = env("BROKER_WS_URL", env("DERIV_PUBLIC_WS_URL", "wss://api.derivws.com/trading/v1/options/ws/public"))
 BROKER_OAUTH_CLIENT_ID = env("BROKER_OAUTH_CLIENT_ID", BROKER_APP_ID)
-BROKER_REDIRECT_URI = env("BROKER_REDIRECT_URI", "")
+BROKER_REDIRECT_URI = env("BROKER_REDIRECT_URI", env("DERIV_REDIRECT_URI", ""))
