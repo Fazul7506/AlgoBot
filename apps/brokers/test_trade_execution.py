757c41eb4bb35614f556581b2c966b9a47820ee8
Fazul7506 <allanopredator687@gmail.com>
2026-08-28 12:22:13 +0300

    test(brokers): seed authoritative market snapshot for execution tests

--- apps/brokers/test_trade_execution.py
+++ apps/brokers/test_trade_execution.py
@@ -8,6 +8,7 @@ from django.test import TransactionTestCase, override_settings
 from apps.brokers.adapters.deriv import DerivAdapter
 from apps.brokers.models import Broker, BrokerAccount, BrokerConnection, ExecutionReport, Order
 from apps.brokers.services import ExecutionEngine
+from apps.market_data.models import MarketSnapshot, MarketSymbol
 
 
 class CanonicalTradeExecutionTests(TransactionTestCase):
@@ -28,6 +29,8 @@ class CanonicalTradeExecutionTests(TransactionTestCase):
         self.account.set_access_token("test-access-token")
         self.account.save(update_fields=["access_token"])
         BrokerConnection.objects.create(broker=self.broker, broker_account=self.account, status="connected")
+        symbol = MarketSymbol.objects.create(symbol="R_100", display_name="Volatility 100", market="Volatility Indices")
+        MarketSnapshot.objects.create(symbol=symbol, last_price="1.25", timestamp=__import__("django.utils.timezone", fromlist=["timezone"]).timezone.now())
 
     @override_settings(BROKER_ORDER_TIMEOUT_SECONDS=2)
     def test_manual_trade_uses_canonical_account_and_returns_execution_report(self):
