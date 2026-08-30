660b7c48bb89f17c7aa992dc4ae3559dd8a4bce8
Fazul7506 <allanopredator687@gmail.com>
2026-08-30 15:10:48 +0300

    refactor: route realtime clients through broker event bus

--- deriv_platform/routing.py
+++ deriv_platform/routing.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from core.realtime import MarketDataConsumer, NotificationConsumer, PortfolioConsumer
+from core.broker_events import BrokerEventConsumer
 
 websocket_urlpatterns = [
-    path("ws/market-data/", MarketDataConsumer.as_asgi()),
-    path("ws/notifications/", NotificationConsumer.as_asgi()),
-    path("ws/portfolio/", PortfolioConsumer.as_asgi()),
+    path("ws/market-data/", BrokerEventConsumer.as_asgi()),
+    path("ws/notifications/", BrokerEventConsumer.as_asgi()),
+    path("ws/portfolio/", BrokerEventConsumer.as_asgi()),
+    path("ws/broker/", BrokerEventConsumer.as_asgi()),
 ]
