408b3d1c15496d1d9f27a386919a65500aea82dd
Fazul7506 <allanopredator687@gmail.com>
2026-08-30 15:11:45 +0300

    refactor: route portfolio and notifications through broker bus

--- deriv_platform/routing.py
+++ deriv_platform/routing.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from core.broker_events import BrokerEventConsumer
-from core.realtime import MarketDataConsumer, NotificationConsumer, PortfolioConsumer
+from core.broker_resource_consumers import NotificationBrokerEventConsumer, PortfolioBrokerEventConsumer
+from core.realtime import MarketDataConsumer
 
 websocket_urlpatterns = [
     path("ws/market-data/", MarketDataConsumer.as_asgi()),
-    path("ws/notifications/", NotificationConsumer.as_asgi()),
-    path("ws/portfolio/", PortfolioConsumer.as_asgi()),
+    path("ws/notifications/", NotificationBrokerEventConsumer.as_asgi()),
+    path("ws/portfolio/", PortfolioBrokerEventConsumer.as_asgi()),
     path("ws/broker/", BrokerEventConsumer.as_asgi()),
 ]
