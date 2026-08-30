f6e6b5ffdcd1f3e2486073f629a2632f424d9adf
Fazul7506 <allanopredator687@gmail.com>
2026-08-30 15:16:00 +0300

    refactor: route market websocket through canonical market bus

--- deriv_platform/routing.py
+++ deriv_platform/routing.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from core.broker_events import BrokerEventConsumer
 from core.broker_resource_consumers import NotificationBrokerEventConsumer, PortfolioBrokerEventConsumer
-from core.realtime import MarketDataConsumer
+from core.market_events import MarketEventConsumer
 
 websocket_urlpatterns = [
-    path("ws/market-data/", MarketDataConsumer.as_asgi()),
+    path("ws/market-data/", MarketEventConsumer.as_asgi()),
     path("ws/notifications/", NotificationBrokerEventConsumer.as_asgi()),
     path("ws/portfolio/", PortfolioBrokerEventConsumer.as_asgi()),
     path("ws/broker/", BrokerEventConsumer.as_asgi()),
