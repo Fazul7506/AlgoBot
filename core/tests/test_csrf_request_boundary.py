34411d3ac5d523ad4458822bc7b2c2be97f38226
Fazul7506 <allanopredator687@gmail.com>
2026-09-04 01:40:02 +0300

    test: add regression coverage for centralized CSRF boundary

---
+++ core/tests/test_csrf_request_boundary.py
@@ -0,0 +1,37 @@
+from django.test import Client, TestCase, override_settings
+from django.urls import reverse
+
+
+class CSRFRequestBoundaryTests(TestCase):
+    def setUp(self):
+        self.client = Client(enforce_csrf_checks=True)
+
+    @override_settings(CSRF_COOKIE_HTTPONLY=False)
+    def test_csrf_bootstrap_sets_browser_cookie(self):
+        response = self.client.get('/api/csrf/', HTTP_ACCEPT='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.json()['ok'])
+        self.assertIn('csrftoken', self.client.cookies)
+
+    def test_api_post_without_csrf_returns_machine_readable_error(self):
+        response = self.client.post(
+            '/api/brokers/connect/',
+            data={'broker_id': 1, 'account_id': 1},
+            content_type='application/json',
+            HTTP_ACCEPT='application/json',
+        )
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.json()['code'], 'CSRF_FAILED')
+        self.assertIn('CSRF verification failed', response.json()['detail'])
+
+    def test_api_post_with_bootstrapped_csrf_passes_csrf_layer(self):
+        self.client.get('/api/csrf/', HTTP_ACCEPT='application/json')
+        token = self.client.cookies['csrftoken'].value
+        response = self.client.post(
+            '/api/brokers/connect/',
+            data={'broker_id': 1, 'account_id': 1},
+            content_type='application/json',
+            HTTP_ACCEPT='application/json',
+            HTTP_X_CSRFTOKEN=token,
+        )
+        self.assertNotEqual(response.status_code, 403)
