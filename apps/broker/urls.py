1280c6f31060debb0f730ce8e82442685c9b292e
Fazul7506 <allanopredator687@gmail.com>
2026-09-04 12:16:44 +0300

    refactor: remove unused legacy broker app

--- apps/broker/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
- path("brokers/", views.brokers), path("brokers/accounts/", views.accounts), path("brokers/connect/", views.connect),
- path("brokers/disconnect/", views.disconnect), path("brokers/status/", views.status), path("brokers/health/", views.health),
-]
