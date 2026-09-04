8644f3211b9f624d31d3bd87fb748c1e8bfef946
Fazul7506 <allanopredator687@gmail.com>
2026-09-04 12:48:35 +0300

    refactor: remove unused audit marker script

--- static/js/audit.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  const eventName = "audit:ready";
-  window.AlgoBotMonitoring = window.AlgoBotMonitoring || { events: [] };
-  window.AlgoBotMonitoring.events.push(eventName);
-  document.dispatchEvent(new CustomEvent(eventName));
-})();
