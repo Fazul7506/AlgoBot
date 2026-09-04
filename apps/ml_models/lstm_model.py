05c710f3b6fae4d259535bbc5fb50263a9f8b962
Fazul7506 <allanopredator687@gmail.com>
2026-09-04 15:33:28 +0300

    chore: remove retired ml_models scaffold

--- apps/ml_models/lstm_model.py
+++
@@ -1,7 +0,0 @@
-class BaseAlgoBotModel:
-    algorithm = 'generic'
-    framework = 'sklearn'
-    def __init__(self, **params): self.params=params; self.is_trained=False
-    def train(self, X, y=None): self.is_trained=True; return {'status':'trained','samples':len(X) if hasattr(X,'__len__') else 0}
-    def predict(self, X): return {'direction':'UP','probability':0.55,'confidence':55.0}
-    def feature_importance(self): return {}
