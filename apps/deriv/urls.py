e7e1e61b6767e103f644aab30cdbfb9edea478e6
Fazul7506 <allanopredator687@gmail.com>
2026-09-04 19:00:22 +0300

    Remove obsolete Deriv URL module

--- apps/deriv/urls.py
+++
@@ -1,3 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [path("deriv/balance/", views.balance), path("deriv/history/", views.history), path("deriv/portfolio/", views.portfolio)]
