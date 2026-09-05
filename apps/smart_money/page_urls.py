9c6412938a98db326fde935239dcd23a511151fd
Fazul7506 <allanopredator687@gmail.com>
2026-09-05 21:10:13 +0300

    Restore Smart Money root page route

--- apps/smart_money/page_urls.py
+++ apps/smart_money/page_urls.py
@@ -2,6 +2,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import path
 
+
 urlpatterns = [
+    path("", login_required(lambda request: render(request, "smart_money/dashboard.html")), name="smart_money_dashboard"),
     path("heatmap/", login_required(lambda request: render(request, "smart_money/heatmap.html")), name="smart_money_heatmap"),
 ]
