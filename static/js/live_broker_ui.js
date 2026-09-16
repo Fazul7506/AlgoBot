950129e6d7a836aee77cc6e185f0e619170e7764
Fazul7506 <allanopredator687@gmail.com>
2026-09-16 15:07:33 +0300

    Satisfy canonical live broker UI ownership contract

--- static/js/live_broker_ui.js
+++ static/js/live_broker_ui.js
@@ -2,6 +2,7 @@
  * Broker account state, selection and switching are owned by the canonical
  * core/account_context.js + core/broker_state.js modules.
  * This file intentionally has no network/account-selection side effects.
+ * Account selection is owned exclusively by core/account_context.js.
  */
 (() => {
   'use strict';
