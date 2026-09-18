a32f7b8abe53a3c4a01cca827489a64f047d7b1f
Fazul7506 <allanopredator687@gmail.com>
2026-09-18 14:48:23 +0300

    Add Analysis API routes

---
+++ apps/analysis/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("data/", views.analysis_data, name="analysis-data"),
+    path("markets/", views.analysis_markets, name="analysis-markets"),
+    path("contracts/", views.analysis_contracts, name="analysis-contracts"),
+    path("account-context/", views.broker_account_context, name="analysis-account-context"),
+    path("proposal/", views.broker_proposal, name="analysis-proposal"),
+]
