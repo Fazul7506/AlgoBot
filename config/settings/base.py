2977822834e6141316ee0fa8df486c6725bb59a2
Fazul7506 <allanopredator687@gmail.com>
2026-09-18 13:04:52 +0300

    Fix middleware configuration syntax

--- config/settings/base.py
+++ config/settings/base.py
@@ -51,7 +51,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware", "whitenoise.middleware.WhiteNoiseMiddleware",
-    "corsheaders.middleware.CorsMiddleware", "django.middleware.common.CommonMiddleware",\n    "core.middleware.wordpress_probe.WordPressProbeMiddleware",
+    "corsheaders.middleware.CorsMiddleware", "django.middleware.common.CommonMiddleware",
+    "core.middleware.wordpress_probe.WordPressProbeMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware", "core.middleware.csrf.APIAwareCsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware", "apps.developer.middleware.DeveloperAPIMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware", "django.middleware.clickjacking.XFrameOptionsMiddleware",
