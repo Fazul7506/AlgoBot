5d8152618f9649fecc40b64a118aba2144a20ac8
Fazul7506 <allanopredator687@gmail.com>
2026-09-18 12:56:06 +0300

    Suppress repetitive WordPress scanner noise in Django logs

--- config/settings/logging.py
+++ config/settings/logging.py
@@ -17,9 +17,9 @@ LOGGING = {
             "style": "{",
         },
     },
-    "handlers": {
-        "console": {"level": "INFO", "class": "logging.StreamHandler", "formatter": "verbose"},
-        "django_file": {"level": "INFO", "class": "logging.handlers.RotatingFileHandler", "filename": LOG_DIR / "django.log", "maxBytes": 10485760, "backupCount": 10, "formatter": "json"},
+    "filters": {\n        "wordpress_probe": {"()": "core.middleware.wordpress_probe.WordPressProbeLogFilter"},\n    },\n    "handlers": {
+        "console": {"level": "INFO", "class": "logging.StreamHandler", "formatter": "verbose", "filters": ["wordpress_probe"]},
+        "django_file": {"level": "INFO", "class": "logging.handlers.RotatingFileHandler", "filters": ["wordpress_probe"], "filename": LOG_DIR / "django.log", "maxBytes": 10485760, "backupCount": 10, "formatter": "json"},
         "oauth_file": {"level": "INFO", "class": "logging.handlers.RotatingFileHandler", "filename": LOG_DIR / "oauth.log", "maxBytes": 10485760, "backupCount": 10, "formatter": "json"},
         "broker_file": {"level": "INFO", "class": "logging.handlers.RotatingFileHandler", "filename": LOG_DIR / "broker.log", "maxBytes": 10485760, "backupCount": 10, "formatter": "json"},
         "trading_file": {"level": "INFO", "class": "logging.handlers.RotatingFileHandler", "filename": LOG_DIR / "trading.log", "maxBytes": 10485760, "backupCount": 10, "formatter": "json"},
@@ -29,7 +29,7 @@ LOGGING = {
     },
     "root": {"handlers": ["console", "django_file", "error_file"], "level": "INFO"},
     "loggers": {
-        "django": {"handlers": ["console", "django_file", "error_file"], "level": "INFO", "propagate": False},
+        "django": {"handlers": ["console", "django_file", "error_file"], "level": "INFO", "propagate": False},\n        "django.server": {"handlers": ["console", "django_file", "error_file"], "level": "INFO", "propagate": False},
         "oauth": {"handlers": ["oauth_file", "error_file"], "level": "INFO", "propagate": True},
         "broker": {"handlers": ["broker_file", "error_file"], "level": "INFO", "propagate": True},
         "market": {"handlers": ["trading_file"], "level": "INFO", "propagate": True},
