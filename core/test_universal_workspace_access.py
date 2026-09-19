6d6a27abbf32b2ec3a435ebc8788960fac5f0fa9
Fazul7506 <allanopredator687@gmail.com>
2026-09-19 18:51:34 +0300

    Update sidebar shell regression cache contract

--- core/test_universal_workspace_access.py
+++ core/test_universal_workspace_access.py
@@ -85,7 +85,7 @@ class UniversalWorkspaceAccessTests(TestCase):
         self.assertIn(".app-shell {\n  display: block !important;", css)
         response = self.client.get("/dashboard/")
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "runtime_recovery.css?v=20260919-shellflow5")
+        self.assertContains(response, "runtime_recovery.css?v=20260919-shellflow6")
 
 
     def test_sidebar_visual_shell_is_fixed_and_brand_spacing_is_stable(self):
@@ -109,7 +109,7 @@ class UniversalWorkspaceAccessTests(TestCase):
         response = self.client.get("/dashboard/")
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "chatgpt_shell.css?v=20260919-sidebarhover5")
-        self.assertContains(response, "runtime_recovery.css?v=20260919-shellflow5")
+        self.assertContains(response, "runtime_recovery.css?v=20260919-shellflow6")
         self.assertContains(response, 'href="/static/icons/favicon.ico"')
  
     def test_runtime_sidebar_layer_is_loaded_after_other_presentation_layers(self):
@@ -121,7 +121,7 @@ class UniversalWorkspaceAccessTests(TestCase):
             html.index("card_alignment.css"),
             html.index("platform_polish.css"),
             html.index("control_polish.css"),
-            html.index("runtime_recovery.css?v=20260919-shellflow5"),
+            html.index("runtime_recovery.css?v=20260919-shellflow6"),
         ]
         self.assertEqual(positions, sorted(positions))
 
