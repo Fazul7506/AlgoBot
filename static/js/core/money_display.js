74649fb2c8cf75f055348f72660e5839ba2fc5a0
Fazul7506 <allanopredator687@gmail.com>
2026-09-22 09:20:59 +0300

    Correct dollar-sign money rendering

--- static/js/core/money_display.js
+++ static/js/core/money_display.js
@@ -28,7 +28,7 @@
     if (!/USD/i.test(text) || !/\\d/.test(text)) return;
     const next = text
       .replace(moneyPattern, '$1$$2')
-      .replace(suffixPattern, '$1');
+      .replace(suffixPattern, '$$1');
     if (next !== text) node.nodeValue = next;
   }
 
