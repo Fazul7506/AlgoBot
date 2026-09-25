f0e1556cc45cfe5cb430eea260b7a27454fa2665
Fazul7506 <allanopredator687@gmail.com>
2026-09-25 10:01:28 +0300

    Delete retired legacy contracts app

--- apps/contracts/repositories.py
+++
@@ -1,4 +0,0 @@
-from .models import Contract
-class ContractRepository:
-    def create(self, **data): return Contract.objects.create(**data)
-    def active(self): return Contract.objects.exclude(status__in=['expired','sold'])
